4cc3ced5663cfcb3f3b75b89fd9612458054d5ce
Martin Kotevski <kotevski.martin.1996@gmail.com>
2018-03-01 13:28:18 +0100

    minor

--- src/app/post-details/post-details.component.ts
+++ src/app/post-details/post-details.component.ts
@@ -15,9 +15,6 @@ export class PostDetailsComponent implements OnInit {
 
   post: Post;
 
-  //tmp
-  bids: Bid[];
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -34,21 +31,6 @@ export class PostDetailsComponent implements OnInit {
       .subscribe(post => {
         this.post = post;
       });
-
-    this.route.paramMap
-      .switchMap((params: ParamMap) => {
-        const id = params.get('id');
-        return this.getBidsByPostIdAsObservable(id);
-      })
-      .subscribe(bids => {
-        this.bids = bids;
-      });
-
   }
 
-  // TEMPORARY SHOULD BE TRANSFERED IN BID MANAGEMENT SERVICE
-  public getBidsByPostIdAsObservable(id: string): Observable<Bid[]> {
-    const queryOnCollection = this.afs.collection('Bid', ref => ref.where('id_user', '==', id));
-    return queryOnCollection.valueChanges();
-  }
 }
