e54bd86305c2df810791f7a1d6a509c85b9a874c
Filip Simonovski <pipe.fanny@gmail.com>
2018-03-01 15:05:32 +0100

    facebookLogin_update

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -9,7 +9,7 @@ import {UserProfileComponent} from "../user-profile/user-profile.component";
 const appRoutes: Routes = [
   { path: 'posts/list', component: PostListComponent },
   { path: 'post/details/:id', component:  PostDetailsComponent },
-  { path: 'user/:id', component:  UserProfileComponent },
+  { path: 'user', component:  UserProfileComponent },
   { path: '',   redirectTo: 'posts/list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
