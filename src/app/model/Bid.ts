c4f873ed714219c027ef0e4ef2f43a258eb92db3
pirana1996 <pirana_coder@hotmail.com>
2018-03-01 22:10:10 +0100

    latest version of bidding without restriction

--- src/app/model/Bid.ts
+++ src/app/model/Bid.ts
@@ -1,10 +1,10 @@
 export class Bid {
-  id_post: number;
-  id_user: number;
+  id_post: string;
+  id_user: string;
   price: number;
   email: string;
 
-  constructor(id_post?: number, id_user?: number, price?: number, email?: string) {
+  constructor(id_post?: string, id_user?: string, price?: number, email?: string) {
     this.id_post = id_post;
     this.id_user = id_user;
     this.price = price;
